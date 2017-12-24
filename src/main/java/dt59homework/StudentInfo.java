43c17afd3fbfa81a416d8ead26d5459367d8bd2d
hupeng <hupeng@LAPTOP-961KT269>
2017-12-24 17:51:10 +0800

    胡鹏 20171224

---
+++ src/main/java/dt59homework/StudentInfo.java
@@ -0,0 +1,29 @@
+/**
+ * Project Name:dt59homework
+ * File Name:studentInfo.java
+ * Package Name:dt59homework
+ * Date:2017年12月24日上午10:38:39
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59homework;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月24日 上午10:38:39 <br/>
+ * @author   hupeng
+ * @version
+ * @see
+ */
+public class StudentInfo {
+	String name;
+	String hobby;
+	int age;
+	double score;
+	public void  print(){
+		System.out.println("学生姓名:"+name+";\t兴趣:"+hobby+";\t年龄:"+age+";\t分数:"+score);
+	}
+	
+	
+
+}
+
