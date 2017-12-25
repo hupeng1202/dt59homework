6827d41232f6ca550860dd6e79cdc2fd35fc411f
hupeng <hupeng@LAPTOP-961KT269>
2017-12-25 08:15:18 +0800

    胡鹏 2017 12 24

---
+++ src/main/java/homework20171224/TestStudentInfo.java
@@ -0,0 +1,30 @@
+/**
+ * Project Name:dt59homework
+ * File Name:testStudentInfo.java
+ * Package Name:dt59homework
+ * Date:2017年12月24日上午11:18:18
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package homework20171224;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月24日 上午11:18:18 <br/>
+ * @author   hupeng
+ * @version
+ * @see
+ */
+public class TestStudentInfo {
+	public static void main(String[] args) {
+	StudentInfo  stu=new StudentInfo();
+		
+	stu.name="胡鹏";
+	stu.hobby="足球";
+	stu.age=21;
+	stu.score=67.2;
+	stu.print();
+	
+
+	}
+}
+
---
+++ src/main/java/homework20171224/dog.java
@@ -0,0 +1,32 @@
+/**
+ * Project Name:dt59homework
+ * File Name:dog.java
+ * Package Name:dt59homework
+ * Date:2017年12月24日下午2:07:38
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package homework20171224;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月24日 下午2:07:38 <br/>
+ * @author   hupeng
+ * @version
+ * @see
+ */
+public class dog {
+	String name;
+	String gender;
+	int age;
+	public void eat(){
+		
+	}
+	public void run(){
+		
+	}
+	public void play(){
+		
+	}
+
+}
+
